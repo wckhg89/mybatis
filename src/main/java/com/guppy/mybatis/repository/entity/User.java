834e42a74ee1c9ad3c836e8e9235a524239325aa
kanghonggu <wckhg89@gmail.com>
2017-06-28 15:20:07 +0900

    lombok 적용

--- src/main/java/com/guppy/mybatis/repository/entity/User.java
+++ src/main/java/com/guppy/mybatis/repository/entity/User.java
@@ -1,52 +1,18 @@
 package com.guppy.mybatis.repository.entity;
 
+import lombok.*;
+
 /**
  * Created by kanghonggu on 2017. 6. 25..
  */
+
+@Data
+@ToString
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
 public class User {
     private Long id;
 
     private String userName;
 
-    public User() {}
-
-    public User(String userName) {
-        this.userName = userName;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other != null && other instanceof User) {
-            if (((User) other).getId() == this.id) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return (userName == null ? userName.length() : 0);
-    }
-
-    @Override
-    public String toString() {
-        return "id[" + id +"] userName[" + userName + "]";
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
